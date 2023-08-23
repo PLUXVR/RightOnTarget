a9fc308121a44fa19ea6f8ab2b6f39f806ff34b3
PLUXVR <pluxar@bk.ru>
2023-08-23 21:35:56 +0300

    Code refactoring MVC

--- RightOnTarget/ViewController.swift
+++ RightOnTarget/ViewController.swift
@@ -7,97 +7,60 @@
 
 import UIKit
 
-
-
 class ViewController: UIViewController {
-
+    //сущность "Game"
+    var game : Game!
+    
     @IBOutlet var sliderOutlet: UISlider!
     
     @IBOutlet var labelOutlet: UILabel!
     
-    var number : Int = .random(in: 1...50)
-    
-    var round : Int = 1
-    
-    var points : Int = 0
-    
-    lazy var secondViewController = getSecondViewController()
-    
-    private func getSecondViewController () -> SecondViewController {
-        let storyboard = UIStoryboard(name: "Main", bundle: nil)
-        let secondViewController = storyboard.instantiateViewController(withIdentifier: "SecondViewController")
-        return secondViewController as! SecondViewController
-    }
-    
+    //MARK: - Жизненный цикл
     
     override func loadView() {
         super.loadView()
-        print("loadView")
-        
         let versionLabel = UILabel(frame: CGRect(x: 20, y: 10, width: 200, height: 20))
-        
-        versionLabel.text = "Версия программы 1.1"
-        
+        versionLabel.text = "Версия программы 1.2"
         self.view.addSubview(versionLabel)
     }
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        print("viewDidLoad")
-        self.number = Int.random(in: 1...50)
-        self.labelOutlet.text = String("Загаданное число: \(self.number)")
+        game = Game(startValue: 1, endValue: 50, rounds: 5)
+        updateLabelWithSecretNumber(newText: String(game.currentSecretValue))
     }
     
-    override func viewWillAppear(_ animated: Bool) {
-        super.viewWillAppear(animated)
-        print("viewWillAppear")
-    }
-
-    override func viewDidAppear(_ animated: Bool) {
-        super.viewDidAppear(animated)
-        print("viewDidAppear")
-    }
-    
-    override func viewWillDisappear(_ animated: Bool) {
-        super.viewWillDisappear(animated)
-        print("viewWillDisappear")
-    }
-    override func viewDidDisappear(_ animated: Bool) {
-        super.viewDidDisappear(animated)
-        print("viewDidDisappear")
-        
-    }
+    //MARK: - Взаимодействие View - Model
     
     @IBAction func checkNumber () {
-        // получаем значение на слайдере
+        
         let numSlider = Int(sliderOutlet.value.rounded())
-        // сравниваем значение с загаданным
-        // и подсчитываем очки
-        if numSlider >= self.number {
-            self.points += 50 - numSlider + self.number
-        } else if numSlider <= self.number {
-            self.points += 50 - self.number + numSlider
-        } else {
-            self.points += 50
-        }
+        game.calculateScore(with: numSlider)
         
-        if self.round == 5 {
-            // выводим информационное окно
-            // с результатами игры
-            let alert = UIAlertController.init(title: "Игра окончена", message: "Вы заработали \(self.points) очков", preferredStyle: .alert)
-            alert.addAction(UIAlertAction.init(title: "Начать заново", style: .default, handler: nil))
-            self.present(alert, animated: true, completion: nil)
-            self.round = 1
-            self.points = 0
+        if game.isGameEnded {
+            
+            showAlertScore(score: game.score)
+            
+            game.restartGame()
+            
         } else {
-            self.round += 1
+            game.startNewRound()
+            
         }
-        
-        // генерируем случайное число
-        self.number = Int.random(in: 1...50)
-        // передаем значение случайного числа в label
-        self.labelOutlet.text = String("Загаданное число: \(self.number)")
+        updateLabelWithSecretNumber(newText: String(game.currentSecretValue))
     }
     
+    //MARK: - Обновление текста загаданного значения
     
+    private func updateLabelWithSecretNumber (newText: String) {
+        self.labelOutlet.text = String("Загаданное число: \(newText)")
+    }
+    
+    //MARK: - Отображение всплывающего окна со счетом
+    
+    private func showAlertScore(score: Int) {
+        let alert = UIAlertController.init(title: "Игра окончена", message: "Вы заработали \(score) очков", preferredStyle: .alert)
+        alert.addAction(UIAlertAction.init(title: "Начать заново", style: .default, handler: nil))
+        self.present(alert, animated: true, completion: nil)
+    }
 }
